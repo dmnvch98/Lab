0d77623e676587f8cc63724df619299d2c0e9e7e
dmn98 <wildehaifisch@gmail.com>
2021-02-16 00:27:23 +0300

    Final fixing

--- src/main/java/Lab/ReaderUtils.java
+++ src/main/java/Lab/ReaderUtils.java
@@ -3,12 +3,12 @@ package Lab;
 import java.util.Scanner;
 
 final class ReaderUtils {
+    private static final Scanner scanner = new Scanner(System.in);
+
     private ReaderUtils(){
         throw new UnsupportedOperationException();
     }
-
     public static String read(){
-        Scanner scanner = new Scanner(System.in);
         return scanner.next();
     }
 }
